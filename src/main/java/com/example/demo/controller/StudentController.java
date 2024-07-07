1100961b86eb337bc624a43a200ced3deb4b0b78
logDevOps“ <logsdevops@gmail.com>
2024-07-07 12:44:44 +0530

    Modified Student class;

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -4,14 +4,7 @@ import com.example.demo.dao.StudentDao;
 import com.example.demo.model.Student;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -22,6 +15,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("api")
 @RequiredArgsConstructor
+@CrossOrigin
 public class StudentController {
 
 	private final StudentDao dao;
@@ -31,6 +25,7 @@ public class StudentController {
 		student.setCreationDate(LocalDateTime.now());
 		student.setLastUpdatedDate(LocalDateTime.now());
 		dao.save(student);
+		log.info("Student Details Register Successfully");
 		return ResponseEntity.ok("student saved successfully");
 	}
 	
